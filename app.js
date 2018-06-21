07730ce73c174797d7fb1cd26589d7beac27c9c4
LaTanya Donaldson <ldonald234@gmail.com>
2018-06-21 18:09:55 +0300

    set the textContent of the to-do li to title, and add the li and checkbox to the DOM

--- app.js
+++ app.js
@@ -6,8 +6,29 @@ function onReady() {
    addToDoForm.addEventListener('submit', () => {
      event.preventDefault();
 
-     //get the text
+     // get the text
      let title = newToDoText.value;
+
+     // create a new li
+     let newLi = document.createElement('li');
+
+     // create a new input
+     let checkbox = document.createElement('input');
+
+     // set the input's type to checkbox
+     checkbox.type = "checkbox";
+
+     // set the title
+     newLi.textContent = title;
+
+     // attach the checkbox to the li
+     newLi.appendChild(checkbox);
+
+     // attach the li to the ul
+     toDoList.appendChild(newLi);
+
+     //empty the input
+     newToDoText.value = '';
 
    });
  }
