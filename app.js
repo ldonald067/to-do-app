ee8fbbc7456cf5043a63bce80ddbd60b34efa5a2
LaTanya Donaldson <ldonald234@gmail.com>
2018-06-23 15:13:29 +0300

    Emptyy onReady function, Add initial state, add createNewToDo() function

--- app.js
+++ app.js
@@ -1,55 +1,12 @@
 function onReady() {
+  const toDos = [];
 
-    const addToDoForm = document.getElementById('addToDoForm');
-    const newToDoText = document.getElementById('newToDoText');
-    const toDoList = document.getElementById('toDoList');
-
-    addToDoForm.addEventListener('submit', event => {
-        event.preventDefault();
-
-        // get the text for the to-do title
-        let title = newToDoText.value;
-
-        // create a new li
-        let newLi = document.createElement('li');
-
-        // create a new input
-        let checkbox = document.createElement('input');
-
-        // set the input's type to checkbox
-        checkbox.type = "checkbox";
-
-        // set the to-do title
-        newLi.textContent = title;
-
-        // attach the checkbox to the li
-        newLi.appendChild(checkbox);
-
-        let compText = document.createTextNode("");
-        newLi.appendChild(compText);
-
-        let delbutton = document.createElement("button");
-        let buttonText = document.createTextNode("Delete");
-        delbutton.addEventListener('click', function () {
-            delbutton.parentNode.setAttribute('style', 'display:none');
-        });
-
-        delbutton.appendChild(buttonText);
-        newLi.appendChild(delbutton);
-
-        // attach the li to the ul
-        toDoList.appendChild(newLi);
-
-        // empty the input
-        newToDoText.value = '';
- 
-
-
-    })
-
-};
+  function createNewToDo () {
+    
+  }
+}
 
 window.onload = function () {
-    // alert("The window has loaded!");
+        // alert("The window has loaded!");
     onReady();
 };
