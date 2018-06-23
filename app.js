5924fcf3852d39bd5f79bda3c29d1a932d12a77b
LaTanya Donaldson <ldonald234@gmail.com>
2018-06-23 15:17:53 +0300

    Add title property, add the new to-do to the toDos array using the push() method

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ function onReady() {
   const toDos = [];
 
   function createNewToDo () {
-    
+    const newToDoText = document.getElementById('newToDoText');
+    toDos.push({
+      title: newToDoText.value,
+      complete: false
+    });
   }
-}
 
 window.onload = function () {
         // alert("The window has loaded!");
