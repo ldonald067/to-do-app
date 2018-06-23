26fd6ae76691172485a53af5a6a9896905a7c576
LaTanya Donaldson <ldonald234@gmail.com>
2018-06-23 16:09:22 +0300

    Complete renderTheUI() function

--- app.js
+++ app.js
@@ -11,6 +11,23 @@ function onReady() {
     newToDoText.value = '';
   }
 
+  function renderTheUI() {
+     const toDoList = document.getElementById('toDoList');
+
+     toDoList.textContent = '';
+
+     toDos.forEach(function(toDo) {
+       const newLi = document.createElement('li');
+       const checkbox = document.createElement('input');
+       checkbox.type = "checkbox";
+
+       newLi.textContent = toDo.title;
+
+       toDoList.appendChild(newLi);
+       newLi.appendChild(checkbox);
+     });
+
+   }
 
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
